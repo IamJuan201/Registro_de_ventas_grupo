2129382f85b7cc5b80c65929b4704fd612fe4c2d
Hector Carvajal <hectordico@hotmail.com>
2026-03-02 08:11:16 -0500

    feat: ya se puede agregar otros productos , pero no saca el total

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 #REGISTRO DE VENTAS
 print("Bienvenido/a, ¡gracias, por preferirnos al comprar!")
-nombre_del_cliente = input("Nombre del cliente: ")
-nombre_del_producto = input("Nombre del producto: ")
-cantidad = int(input("ingrese la cantidades del producto: "))
-precio = float(input("ingrese el valor unitario del producto: "))
 
-sub_total= cantidad * precio 
-iva = sub_total * 0.19
-total_de_venta = sub_total + iva
-print(f"hola {nombre_del_cliente}, su total es: {total_de_venta} ¡gracias por su compra!")
+nombre_del_cliente = input("Nombre del cliente: ")
+
+while True:     
+    nombre_del_producto = input("Nombre del producto: ")
+    cantidad = int(input("ingrese la cantidades del producto: "))
+    precio = float(input("ingrese el valor unitario del producto: "))
+
+    sub_total= cantidad * precio 
+    print(f"hola {nombre_del_cliente}, su cuenta va: {sub_total}")
+ 
+    continuar = input("¿Desea agregar otro producto? (si/no): ").lower()
+    
+    if continuar == "no":
+        break   # rompe el ciclo
+    
+
+    
+   
 
 
